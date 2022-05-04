7a2f068c1c83dfc4d3bdc5b24f4faa686a15ccd4
rbqhr <rbqhr5452@naver.com>
2022-05-05 00:24:59 +0900

    수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="home">
         <p>훌륭한 사람보다는 구석이 있는 사람이 되는 것.</p>
         <a href="http://jhc9639.blog.me" target="_blank" rel="큰돌의 터전">
-            <img src="../assets/logo.png" alt="로고">
+            <img src="../assets/logo.png" alt="로고">            
         </a>
     </div>
 </template>
